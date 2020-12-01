003126bf85e4c6b4847d2b9ee4eb92f911cc0f34
Nakagawakeita <nakagawakeita10@gmail.com>
2020-12-01 13:01:27 +0900

    モデルインポート

--- Assets/Script/StartShot.cs
+++ Assets/Script/StartShot.cs
@@ -10,13 +10,13 @@ public class StartShot : MonoBehaviour
     void Start()
     {
         transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
-        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * (500 + (LevelManager.level * addSpeed)));
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * (200 + (LevelManager.level * addSpeed)));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, transform.rotation.y + 1, 0);
     }
 
     public void BallDestroy()
