ae7d94798c74601b590c53036a1beae78cc7ae78
Nakagawakeita <nakagawakeita10@gmail.com>
2020-12-07 21:19:20 +0900

    イラスト仮、マウスPrefab化

---
+++ Assets/Script/MouseController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public float move = 0.03f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, this.move);
+        
+        if (transform.position.x < -4.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/MouseGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseGenerator : MonoBehaviour
+{
+    public GameObject mousePrefab;
+    float span = 3.0f;
+    float delta = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject mouse = Instantiate(mousePrefab) as GameObject;
+            float x = Random.Range(7, 0);
+            float z = Random.Range(-4, 4);
+            mouse.transform.position = new Vector3(x, 0, z);
+        }
+    }
+}
